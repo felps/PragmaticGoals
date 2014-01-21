930a4afade29ce9b0feba0c25b4ef1521f27e744
Felipe Guimarães <felipepg@ime.usp.br>
2014-01-21 16:42:21 -0200

    Class CGM implemented plus tests

--- src/main/java/cgm/Goal.java
+++ src/main/java/cgm/Goal.java
@@ -1,5 +1,37 @@
 package cgm;
 
-public class Goal {
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.print.attribute.HashAttributeSet;
+
+public class Goal extends Dependency{
+
+	HashSet<Dependency> dependencies;
+	
+	public Goal() {
+		dependencies = new HashSet();
+	}
+	
+	public void addDependency(Dependency goal) {
+		dependencies.add(goal);
+	}
+
+	public Set<Dependency> getDependencies() {
+		return dependencies;
+	}
+
+	public Set<Dependency> getApplicableDependencies(Context context) {
+		
+		HashSet<Dependency> applicableDeps = new HashSet<Dependency>();
+		
+		for (Dependency dep : dependencies) {
+			if(dep.getApplicableContext() == context){
+				applicableDeps.add(dep);
+			}
+		}
+		return applicableDeps;
+	}
 
 }
--- src/test/java/cgm/CGMTest.java
+++ src/test/java/cgm/CGMTest.java
@@ -2,6 +2,8 @@ package cgm;
 
 import static org.junit.Assert.*;
 
+import java.util.HashSet;
+
 import org.junit.Test;
 
 public class CGMTest {
@@ -12,6 +14,50 @@ public class CGMTest {
 		Goal root = new Goal();
 		cgm.setRoot(root);
 		assertEquals(cgm.getRoot(),root);
+	}
+	
+	@Test
+	public void shouldGetDependencies(){
+		CGM cgm = new CGM();
+		Goal root = new Goal();
+		
+		Task task = new Task();
+		Goal goal = new Goal();
+		Delegation delegation = new Delegation();
+		
+		root.addDependency(task);
+		root.addDependency(goal);
+		root.addDependency(delegation);
+		
+		HashSet<Dependency> deps = new HashSet();
+		deps.add(delegation);
+		deps.add(goal);
+		deps.add(task);
+		
+		assertTrue(deps.containsAll(root.getDependencies()));
+	}
+	
+	@Test
+	public void shouldGetApplicableDependencies(){
+		CGM cgm = new CGM();
+		Goal root = new Goal();
+		
+		Context context = new Context();
+		
+		Task task = new Task();
+		Goal goal = new Goal();
+		Delegation delegation = new Delegation();
+		
+		task.setApplicableContext(context);
+		
+		root.addDependency(task);
+		root.addDependency(goal);
+		root.addDependency(delegation);
+		
+		HashSet<Dependency> deps = new HashSet();
+		deps.add(task);
+		
+		assertTrue(deps.containsAll(root.getApplicableDependencies(context)));
 	}
 
 
