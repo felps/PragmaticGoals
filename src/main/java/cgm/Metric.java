18d4998adf033e45d47c8f638f6141d142b6b609
Felipe Pontes <fpontes100@gmail.com>
2014-09-20 11:25:53 -0300

    Renamed metrics

--- src/main/java/cgm/Metric.java
+++ src/main/java/cgm/Metric.java
@@ -2,9 +2,9 @@ package cgm;
 
 public class Metric {
 
-	public static final String METERS = "meters";
-	public static final String SECONDS = "meters";
-	public static final String MINUTES = "meters";
-	public static final String DISTANCE_ERROR = "meters";
+	public static final String METERS = "METERS";
+	public static final String SECONDS = "SECONDS";
+	public static final String MINUTES = "MINUTES";
+	public static final String DISTANCE_ERROR = "DISTANCE";
 
 }
