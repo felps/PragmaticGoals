92dd56d930539dffd3ea76a7de7cfed9aea5ef89
Felipe Pontes <fpontes100@gmail.com>
2014-09-20 12:00:17 -0300

    Added Goal.OR and Goal.AND boolean static constants to Goal

--- src/main/java/cgm/Goal.java
+++ src/main/java/cgm/Goal.java
@@ -6,6 +6,9 @@ import cgm.util.YamlHandler;
 
 public class Goal extends Refinement{
 
+	public final static boolean OR = true;
+	public final static boolean AND = false;
+	
 	public Goal(boolean isOrDecomposition) {
 		dependencies = new HashSet<Refinement>();
 		this.isOrDecomposition = isOrDecomposition;
