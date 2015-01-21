d46c60ae1e7d142c1115e803e45958b307c556d5
Felipe Pontes <fpontes100@gmail.com>
2015-01-21 00:21:19 -0200

    Minor modifications

--- src/test/java/experiment/ContextSweep.java
+++ src/test/java/experiment/ContextSweep.java
@@ -17,29 +17,28 @@ public class ContextSweep {
 	private int contextSet;
 	private long limit;
 
-
 	@Test
-	public void shouldPrintAllContextSets(){
+	public void shouldPrintAllContextSets() {
 		for (Context context : generateNextContextSet(4)) {
 			System.out.println(context.getName());
 		}
 	}
-	
+
 	@Test
-	public void shouldGenerateAllContextSetsForSeveralContextSetSizes(){
-		for(int i=0;i<10;i++){
-			assertEquals(Math.pow(2, i)-1, generateNextContextSet(i));
+	public void shouldGenerateAllContextSetsForSeveralContextSetSizes() {
+		for (int i = 0; i < 10; i++) {
+			assertEquals(Math.pow(2, i) - 1, generateNextContextSet(i));
 		}
 	}
-	
-	//@Test
+
+	// @Test
 	public void scalabilityTestModelAndContextSize() {
 
 		System.out.println("Scalability Evaluation - Varying Model and Context amounts");
 		System.out.println("Experiment executed on " + (new Date()).toString());
 
 		executeScientificalEvaluation("", 1, 10);
-		
+
 		for (int contexts = 1; contexts < 30; contexts++) {
 			for (int model = 100; model < 10000; model += 100) {
 				executeScientificalEvaluation("", contexts, model);
@@ -49,50 +48,53 @@ public class ContextSweep {
 
 	private void executeScientificalEvaluation(String experimentId, int contextAmount, int modelSize) {
 		{
-			long accumulated = 0;
-			boolean achievable = false;
 			int executions = 0;
 
 			RandomCGMGenerator cgmFactory = new RandomCGMGenerator();
 
 			contextSet = 1;
-			
-			for (int i = 0; i < 100; i++) {
+			for (int j = 0; j < 100; j++) {
+				long limit = (long) Math.pow(2, contextAmount);
+				executions = 0;
 				// Setup Model
-
 				CGM cgm = cgmFactory.generateRandomCGM(modelSize, contextAmount);
 
-				executions = 0;
-				for (int j = 0; j < 100; j++) {
-					Set<Context> current = generateNextContextSet();
+				long start = System.nanoTime();
+				for (long i = 0; i < limit; i++) {
+					// To disregard the time of setting up the next context, we
+					// calculate the time it took to do so and add it to the
+					// start time
+					long startPause = System.nanoTime();
+					Set<Context> current = generateNextContextSet(contextAmount);
+					long pauseLength = System.nanoTime() - startPause;
+					start += pauseLength;
+
 					// Execute test
-					long start = System.nanoTime();
 					cgm.isAchievable(current, null);
-					if((System.nanoTime() - start)>60000000000l){
+					if ((System.nanoTime() - start) > 60000000000l) {
 						break;
 					}
 					executions++;
 				}
+				// Reset the context set index
+				contextSet = 0;
 			}
+			double totalPossibleContextSets = Math.pow(2, 50) - 1;
+			// Executions performed over the amount of context sets times 100
+			double parameterSweepCoverage = (executions / totalPossibleContextSets) * 100;  
+
 			// Print result
-			double parameterSweepCoverage = (executions/(Math.pow(2, 50)-1))*100; // Time in nanosseconds for each execution
-			//long timeInMs = accumulated/1000; // Time in milliseconds
-			
 			System.out.print(experimentId + " ");
-			
-			System.out.println(modelSize + " " + contextAmount + " "+ parameterSweepCoverage);
+			System.out.println(modelSize + " " + contextAmount + " " + parameterSweepCoverage);
 		}
 	}
 
-
 	private Set<Context> generateNextContextSet(int contextAmount) {
 		HashSet<Context> contexts = new HashSet<Context>();
-		limit = (long) Math.pow(2l, contextAmount);
-		for (long i= 2; i <= limit; i*=2) {
-			System.out.println("Context set: "+contextSet);
-			if(contextSet%i==0){
+		limit = (long) Math.pow(2, contextAmount);
+		for (long i = 2; i <= limit; i *= 2) {
+			if (contextSet % i == 0) {
 				contexts.add(new Context("c" + i));
-				System.out.println("c"+i);
 			}
 		}
 
