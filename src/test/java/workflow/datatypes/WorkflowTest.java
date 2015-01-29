d22ab222a66df3789a2fe066654bb5f62b6e4a4a
Felipe Pontes <fpontes100@gmail.com>
2015-01-29 11:26:14 -0200

    Testing start and end of workflows

--- src/test/java/workflow/datatypes/WorkflowTest.java
+++ src/test/java/workflow/datatypes/WorkflowTest.java
@@ -1,31 +1,84 @@
 package workflow.datatypes;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
 
 import org.junit.Test;
-
-import workflow.datatypes.Workflow;
-import workflow.datatypes.WorkflowNode;
 
 public class WorkflowTest {
 
 	@Test
-	public void newNodesMustBeCreatedConnected() {
+	public void newNodesMustBeCreatedConnectedToStart() {
 		WorkflowNode node1 = new WorkflowNode("Node 1");
 
 		Workflow wf = new Workflow(node1);
 
 		for (int i = 0; i < 100; i++) {
-			int found = 0;
 			WorkflowNode node2 = new WorkflowNode("Node 2");
 			wf.addNode(node2);
 
-			for (WorkflowNode node : wf.getNodes()) {
-				if (node.getEdges().contains(node2)) {
-					found = 1;
-				}
-			}
-			assertEquals(1, found);
+			assertTrue(wf.getStart().getEdges().contains(node2));
 		}
+	}
+
+	@Test
+	public void newNodesMustBeCreatedConnectedToEnd() {
+		WorkflowNode node1 = new WorkflowNode("Node 1");
+
+		Workflow wf = new Workflow(node1);
+
+		for (int i = 0; i < 100; i++) {
+			WorkflowNode node2 = new WorkflowNode("Node 2");
+			wf.addNode(node2);
+
+			assertTrue(wf.getLastNodes().contains(node2));
+		}
+	}
+
+	@Test
+	public void nodesWithOutgoingEdgesMustNotBeConnectedToEnd() {
+		WorkflowNode node1 = new WorkflowNode("Node 1");
+
+		Workflow wf = new Workflow(node1);
+		WorkflowNode node2 = new WorkflowNode("Node 2");
+		WorkflowNode node3 = new WorkflowNode("Node 3");
+
+		node1.addEdge(node2);
+		node2.addEdge(node3);
+
+		assertFalse(wf.getLastNodes().contains(node1));
+		assertFalse(wf.getLastNodes().contains(node2));
+	}
+
+	@Test
+	public void nodesWithIncomingEdgesMustNotBeConnectedToStart() {
+
+		WorkflowNode node1 = new WorkflowNode("Node 1");
+		WorkflowNode node2 = new WorkflowNode("Node 2");
+		WorkflowNode node3 = new WorkflowNode("Node 3");
+
+		Workflow wf = new Workflow(node1);
+
+		node1.addEdge(node2);
+		node2.addEdge(node3);
+
+		assertFalse(wf.getStart().getEdges().contains(node2));
+		assertFalse(wf.getStart().getEdges().contains(node3));
+	}
+
+	@Test
+	public void everyNodeShouldBeConnectedToStartUnlessOtherwiseStated() {
+		ArrayList<WorkflowNode> nodes = new ArrayList<WorkflowNode>();
+		for (int i = 0; i < 10; i++) {
+			nodes.add(new WorkflowNode("t" + i));
+		}
+		Workflow wf = new Workflow(nodes.get(0));
+		for (int i = 1; i < nodes.size(); i++)
+			wf.addNode(nodes.get(i));
+
+		assertTrue(wf.getStart().getEdges().containsAll(nodes));
+
 	}
 }
