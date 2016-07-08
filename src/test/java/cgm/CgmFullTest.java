4ff4cfa14052164962b0243a841568dc2de62ae8
Felipe Guimarães <fpontes100@gmail.com>
2016-07-08 10:17:38 -0300

    Updated main development test suite

--- src/test/java/cgm/CgmFullTest.java
+++ src/test/java/cgm/CgmFullTest.java
@@ -7,9 +7,9 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({CGMTest.class, GoalTest.class, PragmaticGoalTest.class, FilterQualityConstraintTest.class,
-		RefinementTest.class, TaskTest.class, ContextTest.class, /*TestEachContext.class, */RandomCGMGeneratorTest.class,
-		WorstCaseCGMGeneratorTest.class, InterpretationTest.class, FilterQualityConstraintTest.class})
+@SuiteClasses({CGMTest.class, ContextTest.class, FilterQualityConstraintTest.class, GoalTest.class, InterpretationTest.class,
+        PlanTest.class, PragmaticGoalTest.class, RefinementTest.class, TaskTest.class,  /*TestEachContext.class, */
+        RandomCGMGeneratorTest.class, WorstCaseCGMGeneratorTest.class})
 public class CgmFullTest {
 
 }
