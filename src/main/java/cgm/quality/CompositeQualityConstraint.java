f86f94ed7ec4ae62950af980ec7f196194b8aed6
Felipe Guimarães <fpontes100@gmail.com>
2016-07-12 14:43:02 -0300

    Description of Composite Quality Constraint

--- src/main/java/cgm/quality/CompositeQualityConstraint.java
+++ src/main/java/cgm/quality/CompositeQualityConstraint.java
@@ -5,6 +5,9 @@ import cgm.metrics.exceptions.DifferentMetricsException;
 
 /**
  * Created by Felipe on 08/07/2016.
+ *
+ * A composite quality constraint regards the workflow execution as a whole and not
+ * just the task at hand.
  */
 public class CompositeQualityConstraint extends QualityConstraint {
 
