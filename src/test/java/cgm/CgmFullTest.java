3e66740e33446bceaed637cec7e8ab598fd0a4b0
Felipe Guimarães <fpontes100@gmail.com>
2016-07-12 14:34:21 -0300

    New Alternative annotation test and reviewed full suite of tests

--- src/test/java/cgm/CgmFullTest.java
+++ src/test/java/cgm/CgmFullTest.java
@@ -1,5 +1,8 @@
 package cgm;
 
+import cgm.runtime.annotations.AlternativeAnnotationTest;
+import cgm.runtime.annotations.CardinalityAnnotationTest;
+import cgm.runtime.annotations.InterleavedAnnotationTest;
 import cgm.runtime.annotations.SequentialAnnotationTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -7,7 +10,9 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({CGMTest.class, ContextTest.class, FilterQualityConstraintTest.class, GoalTest.class, InterpretationTest.class,
-        PlanTest.class, PragmaticGoalTest.class, RefinementTest.class, TaskTest.class, SequentialAnnotationTest.class /*TestEachContext.class, */})
+        PlanTest.class, PragmaticGoalTest.class, RefinementTest.class, TaskTest.class, SequentialAnnotationTest.class,
+        InterleavedAnnotationTest.class, AlternativeAnnotationTest.class, CardinalityAnnotationTest.class
+        /*TestEachContext.class, */})
 public class CgmFullTest {
 
 }
---
+++ src/test/java/cgm/runtime/annotations/AlternativeAnnotationTest.java
@@ -0,0 +1,53 @@
+package cgm.runtime.annotations;
+
+import cgm.Refinement;
+import cgm.Task;
+import cgm.workflow.Plan;
+import cgm.workflow.WorkflowTask;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Felipe on 12/07/2016.
+ * <p>
+ * Alternative is an annotation which allows any one refinement to be chosen.
+ * Never more, never less
+ */
+public class AlternativeAnnotationTest {
+    @Test
+    public void shouldReturnNPlansForAnOrDecompositionsTest() throws Exception {
+        Task t1 = new Task("T1");
+        Task t2 = new Task("T2");
+
+        AlternativeAnnotation seq = new AlternativeAnnotation();
+        seq.includeRefinement(t1, 0);
+        seq.includeRefinement(t2, 1);
+
+        Plan plan1 = new Plan(t1);
+        Plan plan2 = new Plan(t2);
+
+        Map<Refinement, Plan> plans;
+        plans = new HashMap<>();
+        plans.put(t1, plan1);
+        plans.put(t2, plan2);
+
+        List<Plan> possible = seq.getPossiblePlans(plans);
+
+        assertEquals(2, possible.size());
+        WorkflowTask wt1 = t1.getWorkflowTask();
+        WorkflowTask wt2 = t2.getWorkflowTask();
+        for (Plan p : possible) {
+            boolean hasT1 = p.getTasks().contains(wt1);
+            boolean hasT2 = p.getTasks().contains(wt2);
+
+            assertTrue(hasT1 || hasT2);
+            assertEquals(1, p.getTasks().size());
+        }
+    }
+}
