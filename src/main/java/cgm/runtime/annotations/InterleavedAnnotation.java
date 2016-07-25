93a25a3b70300cdb1f68aea1d6714e35292e0f6d
Felipe Guimarães <fpontes100@gmail.com>
2016-07-25 10:07:55 -0300

    Removed the goal annotations treatment for AND/OR Goals due to new interpretation of Dalpiaz's goal annotations

--- src/main/java/cgm/runtime/annotations/InterleavedAnnotation.java
+++ src/main/java/cgm/runtime/annotations/InterleavedAnnotation.java
@@ -1,6 +1,5 @@
 package cgm.runtime.annotations;
 
-import cgm.Goal;
 import cgm.Refinement;
 import cgm.workflow.Plan;
 import org.apache.logging.log4j.LogManager;
@@ -24,41 +23,19 @@ public class InterleavedAnnotation extends RuntimeAnnotation {
     @Override
     public List<Plan> getPossiblePlans(Map<Refinement, Plan> approaches) {
         List<Plan> list = new ArrayList<Plan>();
-        if (goalType == Goal.AND) {
-            list.add(getPlan(approaches.values()));
-        } else {
-            if (goalType == Goal.AND) {
-                list.add(getPlan(approaches.values()));
-            } else {
-                Collection<Plan> consideredApproaches = new ArrayList<>();
-                Plan firstRefinementPlan = approaches.get(getRefinements().get(0));
-                Plan secondRefinementPlan = approaches.get(getRefinements().get(1));
-                consideredApproaches.add(firstRefinementPlan);
-                list.add(getPlan(consideredApproaches));
-
-                consideredApproaches.add(secondRefinementPlan);
-                list.add(getPlan(consideredApproaches));
-
-                consideredApproaches.clear();
-                consideredApproaches.add(secondRefinementPlan);
-                list.add(getPlan(consideredApproaches));
-            }
-        }
+        list.add(getPlan(approaches.values()));
         logger.debug("I am the interleaved experiment.runtime annotation and I have devised " + list.size() + " plans ");
 
         return list;
     }
 
-    private Plan getPlan(Collection<Plan> approaches) {
+    private Plan getPlan(Collection<Plan> refinementPlans) {
         Plan completePlan = new Plan();
-
         completePlan.setAchievable(true);
-
-        logger.debug("I am the interleaved experiment.runtime annotation and I have been given " + approaches.size() + " plans");
-        for (Plan refinementPlan : approaches) {
+        logger.debug("I am the interleaved experiment.runtime annotation and I have been given " + refinementPlans.size() + " plans");
+        for (Plan refinementPlan : refinementPlans) {
             completePlan.addParallel(refinementPlan);
         }
-
         return completePlan;
     }
 
--- src/main/java/cgm/runtime/annotations/SequentialAnnotation.java
+++ src/main/java/cgm/runtime/annotations/SequentialAnnotation.java
@@ -28,42 +28,22 @@ public class SequentialAnnotation extends RuntimeAnnotation {
         List<Plan> list = new ArrayList<Plan>();
 
         Plan fullPlan;
-        if (goalType == Goal.AND) {
             consideredRefinements = getRefinements();
             fullPlan = getPlan(approaches, consideredRefinements);
             logger.debug("I am SequentialAnnotation and I found a plan for this refinement with {} tasks.", fullPlan.getTasks().size());
             logger.debug("I am SequentialAnnotation and the plan achievability is " + fullPlan.isAchievable());
             list.add(fullPlan);
             return list;
-        } else {
-            consideredRefinements = new ArrayList<Refinement>();
-            consideredRefinements.add(getRefinements().get(0));
-            fullPlan = getPlan(approaches, consideredRefinements);
-            list.add(fullPlan);
-            consideredRefinements.clear();
-
-            consideredRefinements.add(getRefinements().get(1));
-            fullPlan = getPlan(approaches, consideredRefinements);
-            list.add(fullPlan);
-            consideredRefinements.clear();
-
-            consideredRefinements.add(getRefinements().get(0));
-            consideredRefinements.add(getRefinements().get(1));
-            fullPlan = getPlan(approaches, consideredRefinements);
-            list.add(fullPlan);
-            consideredRefinements.clear();
-
-            return list;
-        }
     }
 
     private Plan getPlan(Map<Refinement, Plan> approaches, List<Refinement> consideredRefinements) {
         Plan fullPlan = new Plan();
         int i;
         fullPlan.setAchievable(true);
+
         for (i = 0; i < consideredRefinements.size(); i++) {
             Plan dependencyPlan = approaches.get(consideredRefinements.get(i));
-            if (dependencyPlan == null && goalType == Goal.AND) {
+            if (dependencyPlan == null) {
                 List<Plan> emptyPlanList = new ArrayList<Plan>(1);
                 Plan unachievable = new Plan();
                 unachievable.setAchievable(false);
