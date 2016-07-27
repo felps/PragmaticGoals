37dcdc848596eeedc7b140f24650bae4435b8d4f
Felipe Pontes <fpontes100@gmail.com>
2016-07-27 19:56:31 -0300

    Evaluation test methods

--- src/experiment/java/experiment/runtime/SensitivityAnalysis/SensitivityAnalysisEvaluation.java
+++ src/experiment/java/experiment/runtime/SensitivityAnalysis/SensitivityAnalysisEvaluation.java
@@ -6,8 +6,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Test;
 import pragmatic.Goal;
-import pragmatic.runtime.annotations.RuntimeAnnotation;
-import pragmatic.runtime.annotations.SequentialAnnotation;
+import pragmatic.runtime.annotations.*;
 
 /**
  * Created by felps on 27/07/16.
@@ -15,37 +14,85 @@ import pragmatic.runtime.annotations.SequentialAnnotation;
 public class SensitivityAnalysisEvaluation {
 
     Logger logger = LogManager.getLogger();
-    private final String experimentsLogFolder = "./logs/experiments/sensitivityAnalysis/";
+
+    private int minModelSize = 500;
+    private int maxModelSize = 10000;
+    private int modelStep = 500;
+
+    private final int repetitionAmount = 10;
+
 
     @Test
     public void sensitivityAnalysisSerialAnd() throws Exception {
+
         RuntimeAnnotation seqRuntimeAnnotation = new SequentialAnnotation();
         AnnotatedGoalGenerator modelGenerator = new AnnotatedGoalGenerator(seqRuntimeAnnotation, Goal.AND);
+        String ID = "SerialAND";
 
+        performAnalysis(modelGenerator, ID, 2);
+    }
+
+
+    @Test
+    public void sensitivityAnalysisParallelAnd() throws Exception {
+
+        RuntimeAnnotation runtimeAnnotation = new InterleavedAnnotation();
+        AnnotatedGoalGenerator modelGenerator = new AnnotatedGoalGenerator(runtimeAnnotation, Goal.AND);
+
+        String ID = "InterleavedAND";
+
+        performAnalysis(modelGenerator, ID, 2);
+    }
+
+
+    @Test
+    public void sensitivityAnalysisAlternativeOR() throws Exception {
+
+        RuntimeAnnotation runtimeAnnotation = new AlternativeAnnotation();
+        AnnotatedGoalGenerator modelGenerator = new AnnotatedGoalGenerator(runtimeAnnotation, Goal.OR);
+
+        String ID = "AlternativeOR";
+
+        performAnalysis(modelGenerator, ID, 2);
+    }
+
+    @Test
+    public void sensitivityAnalysisTry() throws Exception {
+
+        RuntimeAnnotation runtimeAnnotation = new TryAnnotation();
+        AnnotatedGoalGenerator modelGenerator = new AnnotatedGoalGenerator(runtimeAnnotation, Goal.OR);
+
+        String ID = "Try";
+
+        performAnalysis(modelGenerator, ID, 3);
+    }
+
+    @Test
+    public void sensitivityAnalysisSerialIteration() throws Exception {
+
+        RuntimeAnnotation seqRuntimeAnnotation = new SequentialAnnotation();
+        AnnotatedGoalGenerator modelGenerator = new AnnotatedGoalGenerator(seqRuntimeAnnotation, Goal.AND);
+        String ID = "SerialAND";
+
+        performAnalysis(modelGenerator, ID, 2);
+    }
+
+
+    private void performAnalysis(AnnotatedGoalGenerator modelGenerator, String ID, int dependenciesPerNode) {
         ScientificalEvaluation evaluation = new ScientificalEvaluation(modelGenerator);
 
-        String ID = "SerialAND";
-        evaluation.experimentId = ID;
-        evaluation.minModelSize = 500;
-        evaluation.maxModelSize = 10000;
-        evaluation.modelStep = 500;
-        evaluation.repetitions = 100;
 
-        logger.trace( "Beginning serial and evaluation");
-        logger.trace( "Beginning serial and evaluation");
-        logger.trace("Beginning serial and evaluation");
-        logger.trace( "Beginning serial and evaluation");
-        logger.trace( "Beginning serial and evaluation");
-        logger.trace( "Beginning serial and evaluation");
+        evaluation.experimentId = ID;
+        evaluation.minModelSize = minModelSize;
+        evaluation.maxModelSize = maxModelSize;
+        evaluation.modelStep = modelStep;
+        evaluation.repetitions = repetitionAmount;
+
+        logger.trace( "Beginning " + ID + " evaluation");
 
         for(evaluation.contextAmount=1;evaluation.contextAmount<=20;evaluation.contextAmount++)
             evaluation.executeScientificalEvaluation(logger);
 
-        logger.trace( "End serial and evaluation");
-        logger.trace( "End serial and evaluation");
-        logger.trace( "End serial and evaluation");
-        logger.trace( "End serial and evaluation");
-        logger.trace( "End serial and evaluation");
-
+        logger.trace( "End " + ID + " evaluation");
     }
 }
